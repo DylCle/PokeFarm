0d102a8a059203adebb09ba9b3f4b8a385ba3aa1
Dylan Cleveland <newrussianguy12@gmail.com>
2022-12-20 00:08:31 -0400

    added site

---
+++ pokemmonsite/js/scripts.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    let element = document.getElementById("myTopnav");
+    if(element.className === "topnav"){
+        element.className += " responsive";
+    }else{
+        element.className = "topnav";
+    }
+  
+}
